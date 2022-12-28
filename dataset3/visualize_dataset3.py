78b8174f1dc4e41174ef2e49f3869872aeddaef8
Ben Agro <bagro@waabi.ai>
2022-12-28 10:40:28 -0500

    wip dataset3

---
+++ dataset3/visualize_dataset3.py
@@ -0,0 +1,15 @@
+#%% imports
+import numpy as np
+import scipy.io
+
+#%% load dataset
+
+dataset3 = scipy.io.loadmat('dataset3.mat')
+
+#%% visualize ground truth
+
+theta_vk_i = dataset3["theta_vk_i"]
+r_i_vk_i = dataset3["r_i_vk_i"]
+
+rho_i_pj_i = dataset3["rho_i_pj_i"]
+# %%
